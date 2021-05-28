1fdbb45245c47def6ceed7d7c4aa2a1296dea27f
jorgeminhalma <jorgeminhalma0@gmail.com>
2021-05-28 19:38:44 +0100

    delete removido meio de pagamento

--- Code/Models/meiosPagamentoModel.js
+++ Code/Models/meiosPagamentoModel.js
@@ -3,29 +3,11 @@ var pool = require("./connection");
 
 module.exports.editarMeioPagamento = async function(body) {
     try {
-
-        
-        console.log(4535453535)
-        console.log(body)
         let sql ="UPDATE payment_methods SET payment_method_card_name = ?, payment_method_card_number = ?, payment_method_expiry_date = ?, payment_method_cvv = ?,  payment_method_selected = ?,  payment_method_ON = ? WHERE payment_method_id=? AND payment_method_user_id=?";
         let result = await pool.query(sql,[body.cardName, body.cardNumber, body.cardExpiry, body.cardCVV, body.selected, body.paymentON, body.cardID, body.cardUserID]);
 
                     
         return {status: 200, data: result};
-    } catch (err) {
-        console.log(err);
-        return {status: 500, data: err};
-    } 
-};
-
-
-
-module.exports.removerMeioPagamento = async function(id) {
-    try {
-        let sql = "UPDATE payment_method SET payment_method_ON = false WHERE payment_method_id = ?";
-        let result = await pool.query(sql, [id]);
-        return {status: 200, data: result};
-
     } catch (err) {
         console.log(err);
         return {status: 500, data: err};
