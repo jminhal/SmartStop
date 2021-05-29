a8757ef75d8da987199c7f783df01327068f8a71
JorgeMinhalma <jorgeminhalma0@gmail.com>
2021-05-29 13:28:51 +0100

    rota do editar parque alterada

--- Code/Models/parquesModel.js
+++ Code/Models/parquesModel.js
@@ -3,7 +3,7 @@ var pool = require("./connection");
 
 module.exports.getParques = async function() {
     try {
-        let sql = "SELECT * FROM parks";
+        let sql = "SELECT * FROM parks WHERE park_ON = true";
         let result = await pool.query(sql);
         if (result.length > 0) {
             return {status: 200, data: result};
@@ -38,29 +38,17 @@ module.exports.getParque = async function(id) {
 };
 
 
-module.exports.removerParque = async function(id) {
+
+
+
+module.exports.editarParque = async function(body) {
     try {
-        let sql = "UPDATE parks SET park_ON = false WHERE park_id = ?";
-        let result = await pool.query(sql, [id]);
-        return {status: 200, data: result};
+        let sql = "UPDATE parks SET park_name = ?, park_types = ?, park_spots = ?, park_latitude = ?, park_longitude = ?, park_localization = ?, park_hour_open = ?, park_hour_close = ?, park_contact = ?, park_email = ?, park_price_hour = ?, park_create_user_id = ?, park_ON=? WHERE park_id = ?";
+        let result = await pool.query(sql, [body.parkName, body.parkSports, body.parkTypes, body.parkLatitude, body.parkLongitude, body.parkLocalizacao, body.parkOpenHour, body.parkCloseHour, body.parkContact, body.parkEmail, body.parkPrice, body.parkUserCreate, body.parkON]);
+        
 
     } catch (err) {
         console.log(err);
         return {status: 500, data: err};
     } 
 };
-
-
-module.exports.editarMeioPagamento = async function(body) {
-    try {
-        let sql = "UPDATE park_name = ?, park_name = ?, park_spots = ?, park_latitude = ?, park_longitude = ?, park_hour_open = ?, park_hour_close = ?, park_contact = ?, park_create_user_id=? WHERE park_id = ?";
-        let result = await pool.query(sql, [body.name, body.sports, body.latitude, body.longitude, body.openHour,body.closeHour,body.contact, body.contact, body.price,body.create_user_id, body.park_id]);
-        return {status: 200, data: result};
-    } catch (err) {
-        console.log(err);
-        return {status: 500, data: err};
-    } 
-};
-
-
-
