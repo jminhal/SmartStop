75614858057237344a8aac52fcccefd9dfb73246
JorgeMinhalma <jorgeminhalma0@gmail.com>
2021-05-29 13:28:16 +0100

    rota delete meio de pagamento removida

--- Code/routes/meiosPagamentoRouter.js
+++ Code/routes/meiosPagamentoRouter.js
@@ -3,12 +3,7 @@ var router = express.Router();
 var meiosPagamentoModel = require('../Models/meiosPagamentoModel.js');
 
 
-//vai remover um meio de pagamento de um certo utilizador
-router.delete('/:id/remover', async function(req, res, next) {
-    let id = req.params.id;
-    let result = await meiosPagamentoModel.removerMeioPagamento(id);
-    res.status(result.status).send(result.data);  
-});
+
 
 //vai editar as informações de um certo meio de pagamento
 router.put('/:id/editar', async function(req, res, next) { 
