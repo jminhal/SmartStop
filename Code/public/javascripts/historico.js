5d51e5c3c3741d2f200cf030507c7cb84fdd2970
JorgeMinhalma <jorgeminhalma0@gmail.com>
2021-06-18 23:37:21 +0100

    ultimas pessoas que passaram park e user historico

--- Code/public/javascripts/historico.js
+++ Code/public/javascripts/historico.js
@@ -1,72 +1,97 @@
 let user = JSON.parse(sessionStorage.getItem("user"));
+let parque = JSON.parse(sessionStorage.getItem("parque"));
 let userID = user.user_id;
-var dateObj = new Date();
+
+
+let userHistorico = JSON.parse(sessionStorage.getItem("userHistorico"));
 
 
 
 window.onload = async function () {
-    let month = dateObj.getUTCMonth() + 1, day = dateObj.getUTCDate(), year = dateObj.getUTCFullYear(), diaAtual;
-    diaAtual=day+"/"+month+"/"+year;
-    let criarNomeTabela=true;
-    let aux;
     document.getElementById("backArrow").href = "account.html";
+    let aux;
 
+    if(userHistorico){
+        console.log("estou a receber info da conta do user")
 
-try {
-    reservas = await $.ajax({
+        try {
+            reservas = await $.ajax({
+        
+                url: "/api/utilizadores/"+userID+"/reservas",
+                method: "get",
+                dataType: "json"
+            });
+            console.log(reservas)
 
-        url: "/api/utilizadores/"+userID+"/reservas",
-        method: "get",
-        dataType: "json"
-    });
-    for (let i in reservas) {
-        let dataReserva =reservas[i].reservation_start_day;
+            if(reservas.length > 0){
 
-        if(diaAtual>dataReserva){
-            if(criarNomeTabela){
-                var h = document.createElement("h2");
-                var t = document.createTextNode("Your H1 text"); // Create a text element 
-                h.appendChild(t); // Append the text node to the H1 element 
-                document.body.appendChild(h); // Append the H1 element to the document body 
-                aux+="<table><tr><th>Nome do parque</th><th>Local</th><th>Dia de pagamento</th><th>Dia da reserva</th><th>Tempo da reserva</th><th>Veiculo</th><th>Meio de pagamento</th><th>Preço</th></tr>";
-                criarNomeTabela=false;
+                aux = "<table><tr><th>Nome do parque</th><th>Local</th><th>Dia de pagamento</th><th>Dia da reserva</th><th>Tempo da reserva</th><th>Veiculo</th><th>Meio de pagamento</th><th>Preço</th></tr>";
+
+                for (let i in reservas) {
+            
+                    aux+="<tr><td>"+reservas[i].park_name+"</td><td>"+reservas[i].park_localization+"</td><td>"+reservas[i].reservation_date+"</td><td>"+reservas[i].reservation_start_day+"</td><td>"+reservas[i].reservation_duration+"</td><td>"+reservas[i].vehicle_model+" - "+reservas[i].vehicle_brand+"</td><td>**** **** **** "+reservas[i].payment_method_card_number.toString().substring(11,15)+"</td><td>"+reservas[i].reservation_duration*reservas[i].park_price_hour+"</td></tr>";
+            
+                }
+
+            
+
             }
-            aux+="<tr><td>"+reservas[i].park_name+"</td><td>"+reservas[i].park_localization+"</td><td>"+reservas[i].reservation_date+"</td><td>"+reservas[i].reservation_start_day+"</td><td>"+reservas[i].reservation_duration+"</td><td>"+reservas[i].vehicle_model+" - "+reservas[i].vehicle_brand+"</td><td>**** **** **** "+reservas[i].payment_method_card_number.toString().substring(11,15)+"</td><td>"+reservas[i].reservation_duration*reservas[i].park_price_hour+"</td></tr>";
-
-        }
-        else{
-            if(criarNomeTabela){
-                var h = document.createElement("h2");
-                var t = document.createTextNode("Your H1 text"); // Create a text element 
-                h.appendChild(t); // Append the text node to the H1 element 
-                document.body.appendChild(h); // Append the H1 element to the document body 
-                aux+="<table><tr><th>Nome do parque</th><th>Local</th><th>Dia de pagamento</th><th>Dia da reserva</th><th>Tempo da reserva</th><th>Veiculo</th><th>Meio de pagamento</th><th>Preço</th></tr>";
-                criarNomeTabela=false;
+           
+        
+        } catch (err) {
+            console.log(err);
+            if (err.status == 404) {
+                alert(err.responseJSON.msg);
             }
-            aux+="<tr><td>"+reservas[i].park_name+"</td><td>"+reservas[i].park_localization+"</td><td>"+reservas[i].reservation_date+"</td><td>"+reservas[i].reservation_start_day+"</td><td>"+reservas[i].reservation_duration+"</td><td>"+reservas[i].vehicle_model+" - "+reservas[i].vehicle_brand+"</td><td>**** **** **** "+reservas[i].payment_method_card_number.toString().substring(11,15)+"</td><td>"+reservas[i].reservation_duration*reservas[i].park_price_hour+"</td></tr>";
-
         }
-        /*+"<td>"+reservas[i].park_localization+"</td>"+
-        +"<td>"+reservas[i].reservation_start_day+"</td>"+
-        +"<td>"+reservas[i].reservation_duration+"</td>"+
-        +"<td>"+reservas[i].vehicle_model+" - "+reservas[i].vehicle_brand+"</td>"+
-        +"<td>**** **** **** "+reservas[i].payment_method_card_number.toString().substring(11,15)+"</td>"+
-        +"<td>"+reservas[i].reservation_duration*reservas[i].park_price_hour+"</td></tr>"; */
 
     }
-    aux += " </table>";
+    
+
+    
+
+        else{  
+            try {
+                reservas = await $.ajax({
+            
+                    url: "/api/parques/"+parque.park_id+"/reservas",
+                    method: "get",
+                    dataType: "json"
+                });
+                console.log(reservas)
+                if(reservas.length > 0){
+                    document.getElementById("nomePagina").innerHTML="Historico do Parque:"+reservas[0].park_name;
+                    
+                    aux = "<table><tr><th>Nome do parque</th><th>Nome da reserva</th><th>Dia da reserva</th><th>Tempo da reserva</th><th>Preço do parque</th><th>veiculo</th><th>Meio de pagamento usado</th></tr>";
+    
+                    for (let i in reservas) {
+                
+                        aux+="<tr><td>"+reservas[i].park_name+"</td><td>"+reservas[i].user_fullname+"</td><td>"+reservas[i].reservation_start_day+"</td><td>"+reservas[i].reservation_duration+"</td><td>"+reservas[i].park_price_hour+"</td><td>"+reservas[i].vehicle_model+" - "+reservas[i].vehicle_brand+"</td><td>"+reservas[i].payment_method_card_number+"</td></tr>";
+                        
+
+                    }
+
+    
+                
+    
+                }
+               
+            
+            } catch (err) {
+                console.log(err);
+                if (err.status == 404) {
+                    alert(err.responseJSON.msg);
+                }
+            }
+    
+        }
+        aux += " </table>";
+        document.getElementById("tabela").innerHTML = aux;
+    
+    
 
 
-} catch (err) {
-    console.log(err);
-    if (err.status == 404) {
-        alert(err.responseJSON.msg);
-    }
-}
 
 
-
-
-    document.getElementById("tabela").innerHTML = aux;
 
 }
