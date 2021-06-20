aa8c05e6dd3024297585101a5fc6c087f426776d
JorgeMinhalma <jorgeminhalma0@gmail.com>
2021-06-20 14:57:03 +0100

    query alterada

--- Code/Models/meiosPagamentoModel.js
+++ Code/Models/meiosPagamentoModel.js
@@ -3,8 +3,12 @@ var pool = require("./connection");
 
 module.exports.editarMeioPagamento = async function(body) {
     try {
-        let sql ="UPDATE payment_methods SET payment_method_card_name = ?, payment_method_card_number = ?, payment_method_expiry_date = ?, payment_method_cvv = ?,  payment_method_selected = ?,  payment_method_ON = ? WHERE payment_method_id=? AND payment_method_user_id=?";
+        let sql ="UPDATE payment_methods SET payment_method_card_name = COALESCE(?,payment_method_card_name), payment_method_card_number = COALESCE(?,payment_method_card_number), payment_method_expiry_date = COALESCE(?,payment_method_expiry_date), payment_method_cvv = COALESCE(?,payment_method_cvv), payment_method_selected = COALESCE(?,payment_method_selected), payment_method_ON = COALESCE(?,payment_method_ON) WHERE payment_method_id=? AND payment_method_user_id=?";
         let result = await pool.query(sql,[body.cardName, body.cardNumber, body.cardExpiry, body.cardCVV, body.selected, body.paymentON, body.cardID, body.cardUserID]);
+
+
+
+
 
                     
         return {status: 200, data: result};
@@ -14,3 +18,15 @@ module.exports.editarMeioPagamento = async function(body) {
     } 
 };
 
+//vai buscar as informações dos tipos de veiculo
+module.exports.getMeioPagamentoSelecionado = async function(id) {
+    try {
+        let sql = "SELECT * FROM payment_methods WHERE payment_method_user_id = ? AND payment_method_selected = true";
+        let result = await pool.query(sql,[id]);
+        return {status: 200, data: result[0]};
+
+    } catch (err) {
+        console.log(err);
+        return {status: 500, data: err};
+    } 
+};
