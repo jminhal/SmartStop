c6fb2b5fac7fdc007279f88b579e6a156c785b3a
JorgeMinhalma <jorgeminhalma0@gmail.com>
2021-06-26 14:19:59 +0100

    alteração da query

--- Code/Models/veiculosModel.js
+++ Code/Models/veiculosModel.js
@@ -4,13 +4,16 @@ var pool = require("./connection");
 module.exports.editarVeiculo = async function(body) {
     try {
         let sql = "UPDATE vehicles SET vehicle_model = ?, vehicle_brand = ?, vehicle_registration = ?, vehicle_registration_date = ?,  vehicle_category = ?, vehicle_selected = ?, vehicle_ON = ? WHERE vehicle_id=? AND vehicle_user_id=?";
-        let result = await pool.query(sql,[body.vModel, body.vBrand, body.vRegistration, body.vDate, body.vCategory, body.vSelected, body.vON, body.vID, body.vUserID]);
+        let result = await pool.query(sql,[body.vModel, body.vBrand, body.vRegistration, body.vDate, body.vCategory, body.vSelected, body.vON, body.veiculoID, body.vUserID]);
+       // console.log(result)
+
         return {status: 200, data: result};
     } catch (err) {
         console.log(err);
         return {status: 500, data: err};
     } 
 };
+
 
 
 //vai os tipos de veiculo
@@ -31,6 +34,7 @@ module.exports.getVeiculoSelecionado = async function(id) {
     try {
         let sql = "SELECT * FROM vehicles WHERE vehicle_user_id = ? AND vehicle_selected = true";
         let result = await pool.query(sql,[id]);
+        console.log(result)
         return {status: 200, data: result[0]};
 
     } catch (err) {
