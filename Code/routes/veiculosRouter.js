354b903e3747e766b7c00e98f58a349127967b66
jorgeminhalma <jorgeminhalma0@gmail.com>
2021-06-26 11:13:20 +0100

    corceção da rota do veiculo

--- Code/routes/veiculosRouter.js
+++ Code/routes/veiculosRouter.js
@@ -8,17 +8,10 @@ var veiculosModel = require('../Models/veiculosModel');
 router.put('/:id/editar', async function(req, res, next) { 
     let id = req.params.id;
     let body = req.body;
-    body.vehicle_id=id;
+    body.vUserID=id;
     let result = await veiculosModel.editarVeiculo(body);
     res.status(result.status).send(result.data);  
 });
-
-//vai os tipos de veiculo
-router.get('/tipoVeiculo', async function(req, res, next) {
-    let result = await veiculosModel.getTipoVeiculo();
-    res.status(result.status).send(result.data);  
-});
-
 
 //vai buscar os veiculos selecionados de um certo utilizadoor
 router.get('/:id/selecionado', async function(req, res, next) {
@@ -26,6 +19,14 @@ router.get('/:id/selecionado', async function(req, res, next) {
     let result = await veiculosModel.getVeiculoSelecionado(id);
     res.status(result.status).send(result.data);  
 
+});
+
+
+
+//vai os tipos de veiculo
+router.get('/tipoVeiculo', async function(req, res, next) {
+    let result = await veiculosModel.getTipoVeiculo();
+    res.status(result.status).send(result.data);  
 });
 
 
