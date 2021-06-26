d197ec99559f72cb3b8c2475854b2ce467b43134
JorgeMinhalma <jorgeminhalma0@gmail.com>
2021-06-26 12:41:44 +0100

    alteracao na parte editar informacao

--- Code/public/javascripts/infoaccount.js
+++ Code/public/javascripts/infoaccount.js
@@ -1,6 +1,10 @@
 let user = JSON.parse(sessionStorage.getItem("user"));
 var utilizadorID= user.user_id;
 var moderador= user.user_moderador;
+var password2;
+
+
+
 
 
 
@@ -37,25 +41,28 @@ function Atualizar(){
 
 //função que irá editar a informação do  para a base de dados 
 async function EditarInformaçãoConta(){
+
+  
     let name = document.getElementById("name").value;
-    let email = document.getElementById("email");
-    let pw = document.getElementById("pw").value;
     let data = document.getElementById("date").value;
     let nif = document.getElementById("nif").value;
     let telemovel = document.getElementById("telemovel").value;
+    let mail =document.getElementById("email").textContent;
 
-    if (email != "" && pw != "" && data != "" && nif != "" && telemovel != "" && name != "") { //Verificar se o input do email não está vazio
+
+    if (pw != "" && data != "" && nif != "" && telemovel != "" && name != ""&& mail != "") { //Verificar se o input do email não está vazio
 
         try {
 
             let info = {
                 fullname: name,
-                email: email,
-                password: pw,
+                email:mail,
+                password: password2,
                 birthday: data,
                 mobile: telemovel,
                 nif: nif
             }
+            console.log(info)
 
             let user = await $.ajax({
               url: "/api/utilizadores/"+utilizadorID+"/editar",
@@ -82,8 +89,9 @@ async function EditarInformaçãoConta(){
     else {
         alert("Preenche todos os campos");
     }
-    alert("Dados alterados com sucesso!");
-    window.location = "account.html";
+    alert("Informação de conta alterada com sucesso! Vamos agora reiniciar a conta");
+    sessionStorage.clear();
+    window.location = "index.html";
 
 
 }
@@ -96,17 +104,17 @@ function carregarDivConfPassword(){
       //mete as os inputs
       aux += "<div class='boxInput'><input type='password' placeholder='A sua senha' class='input' id='confpw'></div>";
       //mete os butttons
-      aux += "<div class='btnBox'><button class='btn' onclick='ConfirmarPassword()'>Confirmar senha</button></div>";
+      aux += "<div class='btnBox' id='btnBox'><button class='btn' onclick='ConfirmarPassword()'>Confirmar senha</button></div>";
   
       boxChild.style.width = "700px";
       confirmPassword.style.display = "flex";
-  
-      document.getElementById("boxChild").innerHTML = aux;
-}
+      document.getElementById("boxChild").innerHTML=aux;
+      document.getElementById("confirmPassword");
+  }
 
 
 function ConfirmarPassword(){
-  let password2= document.getElementById("confpw").value;
+  password2= document.getElementById("confpw").value;
   CarregarInfo(password2);
 }
 
@@ -124,21 +132,25 @@ async function CarregarInfo(password2){
             method: "get",
             dataType: "json"
         });
-  
         if (utilizador.success == 1) {
           document.getElementById("name").value = user.user_fullname;
           document.getElementById("email").innerHTML = user.user_email;
-          document.getElementById("pw").value = "******";
+          document.getElementById("pw").value = "*****";
           document.getElementById("date").value = user.user_birthday;
           document.getElementById("nif").value = user.user_nif;
           document.getElementById("telemovel").value = user.user_mobile;
+          document.getElementById("lableInput");
+          document.getElementById("btnsBox");
+          document.getElementById("btnBox");
+          document.getElementById("boxLabel");
+          document.getElementById("boxChild");
 
           lableInput.style.display = "flex";
           btnsBox.style.display = "flex";
           btnBox.style.display = "none";
           boxLabel.style.display = "flex";
           boxChild.style.display = "none";
-  
+
         }
   
         else {
