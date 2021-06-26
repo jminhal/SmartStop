56b7682d327fe5ca7deceb185d81b596b4e9c6aa
JorgeMinhalma <jorgeminhalma0@gmail.com>
2021-06-26 12:41:59 +0100

    alteracao na query

--- Code/Models/utilizadoresModel.js
+++ Code/Models/utilizadoresModel.js
@@ -6,6 +6,7 @@ module.exports.getUserByEmail = async function(email) {
     try {
         let sql = "SELECT user_id, user_fullname, user_email, user_password, DATE_FORMAT(user_birthday, '%Y-%m-%d') AS 'user_birthday', user_mobile, user_nif, user_moderador, user_active FROM users WHERE user_email = ?";
         let utilizador = await pool.query(sql, [email]);
+        console.log(utilizador)
         if (utilizador.length > 0) {
 
             return {status: 200, data: utilizador[0]};
@@ -186,6 +187,7 @@ module.exports.novoVeiculo = async function(body) {
 
 //vai editar as informações de conta de um certo utilizador
 module.exports.editarUser = async function(body) {
+    console.log(body)
     try {
         let sql = "UPDATE users SET user_fullname = ?, user_email = ?, user_password = ?, user_birthday = ?,  user_mobile = ?, user_nif = ? WHERE user_id=?";
         let result = await pool.query(sql, [body.fullname, body.email, body.password, body.birthday, body.mobile, body.nif, body.userID]);
